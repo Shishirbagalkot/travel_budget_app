0babad266c1c41c15c0352cd0ee8e1da7b290b59
Shishir bagalkot <bshishir2000@gmail.com>
2021-05-04 14:59:18 +0530

    auth_service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  //signifies if the user change their login state(sign in or sign out)
+  Stream<String> get autheStateChanges => _firebaseAuth.authStateChanges().map(
+    (User user) => user?.uid,
+  );
+
+  //email and password sign-up
+  Future<String> createUserWithEmailAndPassword(String email, String password, String name) async{
+    final currentUser = await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email, 
+      password: password
+    );
+
+    //update username
+    await _firebaseAuth.currentUser.updateProfile(displayName: name);
+    return currentUser.user.uid;
+  }
+  //email and password sign-in
+  Future<String> signInWithEmailAndPassword(String email, String password) async{
+    return (await _firebaseAuth.signInWithEmailAndPassword(
+      email: email, 
+      password: password)
+    ).user.uid;
+  }
+
+  //sign out
+  signOut() {
+    return _firebaseAuth.signOut();
+  }
+}
